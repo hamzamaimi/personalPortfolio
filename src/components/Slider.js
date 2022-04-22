1c0898191c4a7cdfc38d9126aacf6c153396a093
hamzamaimi <hamzamaimi0901@gmail.com>
2022-04-22 12:25:51 +0200

    creata nav bar per desktop e mobile

---
+++ src/components/Slider.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Slider = (props) => {
+    return (
+        <>
+            <div id="sliderChild" className="container-fluid">
+                <div className="row align-self-center">
+                    <div className="col-lg-4"></div>
+                    <div className="col-lg-4 text-center">metà centro</div>
+                    <div className="col-lg-4"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Slider
