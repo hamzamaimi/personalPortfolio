6c60d74ad9d5851d596dc6a13e056bc47547b9de
hamzamaimi <hamzamaimi0901@gmail.com>
2022-04-26 10:58:01 +0200

    finita la pagina about

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const About = () => {
         }
         if (!screenHeightNoNavbar) {
             setScreenHeightNoNavbar(localStorage.getItem('screenHeightNoNavbar'))
-            setScreenHeightNoNavbarCss({ height: localStorage.getItem('screenHeightNoNavbar') + 'px' })
+            setScreenHeightNoNavbarCss({ maxHeight: localStorage.getItem('screenHeightNoNavbar') + 'px' })
         }
         if (!navbarHeight) {
             setnavbarHeight(localStorage.getItem('navbarHeight'))
@@ -71,31 +71,56 @@ const About = () => {
                     <div style={marginTopCssRule} id="aboutPageContent" className="row">
                         <div className="col-lg-1"></div>
                         <div className="col-lg-5 d-none d-lg-block">
-                            <img id="aboutImage" src="images/developer.png" alt="developer image" />
+                            <img id="aboutImage" src="images/developer.png" alt="developer" />
                         </div>
                         <div className="col-12 col-lg-5">
                             <div className="row">
-                                <div className="col-12">
+                                <div id="aboutTitle" className="col-12 verticalMargin5">
                                     About Me
                                 </div>
-                                <div className="col-12">
+                                <div style={{ marginBottom: '5%' }} className="col-12 aboutContent">
                                     little description
                                 </div>
-                                <div className="col-4">Name:</div>
-                                <div className="col-8">Hamza Maimi</div>
-                                <div className="col-4">Date of birth:</div>
-                                <div className="col-8">March 28, 1999</div>
-                                <div className="col-4">Address</div>
-                                <div className="col-8">Modena 41059 Italy</div>
-                                <div className="col-4">Zip code:</div>
-                                <div className="col-8">41053</div>
-                                <div className="col-4">Email:</div>
-                                <div className="col-8">hamzamaimi@gmail.com</div>
-                                <div className="col-4">Phone</div>
-                                <div className="col-8">+39 3392579431</div>
+                                <div className="col-4 verticalMargin2 font-weight-bold">Name:</div>
+                                <div className="col-8 aboutContent verticalMargin2">Hamza Maimi</div>
+                                <div className="col-4 verticalMargin2 font-weight-bold">Date of birth:</div>
+                                <div className="col-8 aboutContent verticalMargin2">March 28, 1999</div>
+                                <div className="col-4 verticalMargin2 font-weight-bold">Address</div>
+                                <div className="col-8 aboutContent verticalMargin2">Modena 41059 Italy</div>
+                                <div className="col-4 verticalMargin2 font-weight-bold">Zip code:</div>
+                                <div className="col-8 aboutContent verticalMargin2">41053</div>
+                                <div className="col-4 verticalMargin2 font-weight-bold">Email:</div>
+                                <div className="col-8 aboutContent verticalMargin2">hamzamaimi@gmail.com</div>
+                                <div className="col-4 verticalMargin2 font-weight-bold">Phone</div>
+                                <div className="col-8 aboutContent verticalMargin2">+39 3392579431</div>
+                                <div className="col-12 verticalMargin5" id="downlaodButtonAbout" >
+                                    <form method="get" action="documents/HamzaMaimiCurriculum.pdf">
+                                        <button style={{ borderRadius: '15px' }} type="button" className="btn btn-primary">
+                                            <a id="downloadAnchorAbout" href="documets/HamzaMaimiCurriculum.pdf" download style={{ fontWeight: 'bold', fontSize: '12px', color: 'white' }}>DOWNLOAD CV</a>
+                                        </button>
+                                    </form>
+                                </div>
+
                             </div>
                         </div>
                         <div className="col-lg-1"></div>
+                    </div>
+
+                </div>
+                <div className="container-fluid verticalMargin5">
+                    <div className="row">
+                        <div className="col-6 col-lg-3 text-center">
+                            <i className="fa-brands fa-java" style={{ fontSize: '100px' }} />
+                        </div>
+                        <div className="col-6 col-lg-3 text-center">
+                            <i className="fa-brands fa-js" style={{ fontSize: '100px' }} />
+                        </div>
+                        <div className="col-6 col-lg-3 text-center">
+                            <i className="fa-brands fa-html5" style={{ fontSize: '100px' }} />
+                        </div>
+                        <div className="col-6 col-lg-3 text-center">
+                            <i className="fa-brands fa-css3-alt" style={{ fontSize: '100px' }} />
+                        </div>
                     </div>
                 </div>
             </>
